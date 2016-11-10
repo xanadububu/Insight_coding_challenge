6b3a05e4d2476f2ec4009ac1fb97833d8f8d3de0
xanadububu <zhiwei.xiao@gmail.com>
2016-11-10 13:00:36 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@
 
 # I'll execute my programs, with the input directory paymo_input and output the files in the directory paymo_output
 javac -d bin -sourcepath src src/Main.java 
-java -cp ./bin Main ./paymo_input/batch_payment.csv ./paymo_input/stream_payment.csv ./paymo_output/output1.txt ./paymo_output/output2.txt ./paymo_output/output3.txt
+java -cp ./bin Main ./paymo_input/batch_payment_t3.csv ./paymo_input/stream_payment_t3.csv ./paymo_output/output_t1.txt ./paymo_output/output_t2.txt ./paymo_output/output_t3.txt
